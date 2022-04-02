5552ecd22996d60564c365ed01a7a7cb66b8f1b1
A-IdrissiHamza <a.idrissihamza@gmail.com>
2022-04-02 18:46:52 +0200

    send data to firebase

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import Month from "./components/Month";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import TasksBar from "./components/TasksBar/TasksBar";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
+const URL = process.env.REACT_APP_URL;
 function App() {
+  const tasks = useSelector((state) => state.tasks);
+  useEffect(() => {
+    fetch(` ${URL} `, {
+      method: "PUT",
+      body: JSON.stringify(tasks),
+    });
+  }, [tasks]);
   return (
     <div className="  flex flex-col h-screen ">
       <Header />
       <main className="flex  h-full">
         <Navbar />
         <Month />
-        <TasksBar/>
+        <TasksBar />
       </main>
     </div>
   );
